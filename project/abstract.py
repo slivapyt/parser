9efeb441d714267360a09cc5cade8b5e87a7ba56
slivapyt <faer2325@gmail.com>
2024-04-09 15:54:24 +0700

    1

--- project/abstract.py
+++ project/abstract.py
@@ -14,3 +14,29 @@ class JobPlatform(ABC):
     @abstractmethod
     def get_value_vacancies(self):
         pass
+
+
+    def format_payment(self, payment_to, payment_from):
+        if not payment_to and not payment_from:
+            payment = "зп не указанна"
+            
+        elif not payment_from:
+            payment = payment_to
+            
+        elif not payment_to:
+            payment = payment_from
+        else:
+            payment = f'{payment_from} - {payment_to}'
+        return payment
+
+
+    def get_value_vacancies(self, pages_count=1, params,):
+        while self.__params["page"] < pages_count:
+            try:
+                values = self.get_request()
+            except Exception:
+                print("ошибка при парсинге")
+                break
+            self.__vacancies.extend(values)
+            self.__params["page"] += 1
+        return self.__vacancies
